06fb51066b2133de799186c20f8ee51cf6e34c4b
Aditya Ghosh <ghoshaditya149@gmail.com>
2025-05-02 18:18:53 +0530

    setup superadmin route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express from "express";
 
+import superAdminRoutes from "./routes/superadmin.routes.js";
+
 const app = express();
 
 const corsOptions = {
@@ -13,5 +15,8 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
+
+// routes
+app.use("/api/v1/superadmin", superAdminRoutes);
 
 export default app;
