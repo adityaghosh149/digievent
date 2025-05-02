216b31120a0fd864f00095c6add19525036370ec
Aditya Ghosh <ghoshaditya149@gmail.com>
2025-05-02 06:48:43 +0530

    add superadmin model

---
+++ src/models/superadmin.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+
+const superAdminSchema = new mongoose.Schema(
+    {
+        super_admin_id: {
+            type: String,
+            default: () => uuidv4(),
+            unique: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        phone_number: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        is_deleted: {
+            type: Boolean,
+            default: false
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const SuperAdmin = mongoose.model("SuperAdmin", superAdminSchema);
+
