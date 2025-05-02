04d40bed4a94e23d64ba0a3ec44126a2db7178b5
Aditya Ghosh <ghoshaditya149@gmail.com>
2025-05-02 19:44:54 +0530

    add password hashing, auth token methods to admin schema

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -65,4 +65,41 @@ const adminSchema = new mongoose.Schema(
     }
 );
 
+adminSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+adminSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+adminSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            role: "Admin",
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+    );
+};
+
+adminSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+    );
+};
+
 export const Admin = mongoose.model("Admin", adminSchema);
