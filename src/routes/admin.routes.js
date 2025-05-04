35581cc45cece0c1da6145b79e738b44c2db2f3e
Aditya Ghosh <ghoshaditya149@gmail.com>
2025-05-04 23:24:56 +0530

    add route for admin login

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { loginAdmin } from "../controllers/admin.auth.controller.js";
 
 const router = Router();
+
+// public route
+router.route("/login").post(loginAdmin);
 
 export default router;
